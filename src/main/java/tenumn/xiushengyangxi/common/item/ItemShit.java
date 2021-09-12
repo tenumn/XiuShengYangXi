33db8f474e999eacbab2fa4ddb9ce70c6c7e5795
uicdb <166466006@qq.com>
2021-09-12 13:29:03 +0800

    写了些奇奇怪怪的注释 Signed-off-by: uicdb <166466006@qq.com>
    Signed-off-by: uicdb <166466006@qq.com>

--- src/main/java/tenumn/xiushengyangxi/common/item/ItemShit.java
+++ src/main/java/tenumn/xiushengyangxi/common/item/ItemShit.java
@@ -9,11 +9,11 @@ public class ItemShit extends Item {
     public ItemShit( ) {
         //简简单单一坨屎，要品尝一些吗
         super(new Properties().
-                setNoRepair().
-                maxStackSize(16).
-                food(new Food.Builder().hunger(1).saturation(0.5f).build()).
-                isImmuneToFire().
-                rarity(Rarity.COMMON).
-                group(ModRegistry.MISCGROUP));
+                setNoRepair().//难道你想修shit
+                maxStackSize(16).//太多了会很臭的
+                food(new Food.Builder().hunger(1).saturation(0.5f).build()).//嘿嘿嘿，吃一口吗
+                isImmuneToFire().//防火
+                rarity(Rarity.COMMON).//一点也不稀有
+                group(ModRegistry.MISCGROUP));//是个杂物呢
     }
 }

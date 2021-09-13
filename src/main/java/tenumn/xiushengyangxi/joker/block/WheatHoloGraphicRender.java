c1f5b4a5b497776c5fbb0f7c6f7311c7530f3355
uicdb <166466006@qq.com>
2021-09-13 21:39:33 +0800

    移动了一个事件类，增加了注释 Signed-off-by: uicdb <166466006@qq.com>
    Signed-off-by: uicdb <166466006@qq.com>

--- src/main/java/tenumn/xiushengyangxi/joker/block/WheatHoloGraphicRender.java
+++ src/main/java/tenumn/xiushengyangxi/joker/block/WheatHoloGraphicRender.java
@@ -4,16 +4,25 @@ import com.mojang.blaze3d.matrix.MatrixStack;
 import net.minecraft.client.Minecraft;
 import net.minecraft.client.renderer.IRenderTypeBuffer;
 import net.minecraft.client.renderer.ItemRenderer;
+import net.minecraft.client.renderer.RenderState;
+import net.minecraft.client.renderer.RenderType;
 import net.minecraft.client.renderer.model.IBakedModel;
 import net.minecraft.client.renderer.model.ItemCameraTransforms;
 import net.minecraft.client.renderer.tileentity.TileEntityRenderer;
 import net.minecraft.client.renderer.tileentity.TileEntityRendererDispatcher;
+import net.minecraft.client.renderer.vertex.DefaultVertexFormats;
 import net.minecraft.item.ItemStack;
 import net.minecraft.util.math.vector.Vector3f;
+import org.lwjgl.opengl.GL11;
+import tenumn.xiushengyangxi.Utils;
 import tenumn.xiushengyangxi.common.ModRegistry;
+
 
 //特殊渲染（未完成）
 public class WheatHoloGraphicRender extends TileEntityRenderer<WheatHolographicTileEntity> {
+    protected static final RenderState.WriteMaskState DEPTH_WRITE = new RenderState.WriteMaskState(false, true);
+    protected static RenderType HOLO_TYPE = RenderType.makeType("holo", DefaultVertexFormats.POSITION_TEX, GL11.GL_LINE_LOOP, 256, RenderType.State.getBuilder().texture(new RenderState.TextureState(Utils.modLoc("textures/item/shit.png"), false, false)).writeMask(DEPTH_WRITE).build(false));
+
     public WheatHoloGraphicRender(TileEntityRendererDispatcher rendererDispatcherIn) {
         super(rendererDispatcherIn);
     }
@@ -29,6 +38,15 @@ public class WheatHoloGraphicRender extends TileEntityRenderer<WheatHolographicT
         ItemStack stack = new ItemStack(ModRegistry.Items.SHIT.get());
         IBakedModel ibakedmodel = itemRenderer.getItemModelWithOverrides(stack, tileEntityIn.getWorld(), null);
         itemRenderer.renderItem(stack, ItemCameraTransforms.TransformType.FIXED, true, matrixStackIn, bufferIn, combinedLightIn, combinedOverlayIn, ibakedmodel);
+       /*
+       IVertexBuilder iVertexBuilder= bufferIn.getBuffer(HOLO_TYPE);
+        iVertexBuilder.pos(0,0,0).tex(0,0).endVertex();
+        iVertexBuilder.pos(0,0,1).tex(0,1).endVertex();
+        iVertexBuilder.pos(1,0,1).tex(1,1).endVertex();
+        iVertexBuilder.pos(1,0,0).tex(1,0).endVertex();
+        */
         matrixStackIn.pop();
+
     }
+
 }
